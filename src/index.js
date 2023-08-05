aba66fcef79001919b4505b0f49d2cf51c6aed59
nastyadev2 <nastagar.kam99@gmail.com>
2023-08-05 16:33:04 +0300

    init: start "human-readable-number" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+  const number_0_9 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const number_10_19 = ['ten', 'eleven', 'tvelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dozens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const houndreds = 'houndred';
 }
